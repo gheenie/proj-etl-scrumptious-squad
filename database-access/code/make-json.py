9f69d1a03e9ea462c497b8ffbe8e096f86d4f5f3
soyborg <alex.leigh.work@gmail.com>
2023-03-21 15:49:52 +0000

    data tables

---
+++ database-access/code/make-json.py
@@ -0,0 +1,45 @@
+from pip._internal import main
+import pg8000
+import json
+ 
+def connection():
+     
+    sql = """SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema='public'
+    AND table_type= 'BASE TABLE';"""
+     
+    conn = pg8000.connect(
+        database='totesys',
+        user='project_user_4',
+        password='LC7zJxE3BfvY7p',
+        host='nc-data-eng-totesys-production.chpsczt8h1nu.eu-west-2.rds.amazonaws.com',
+        port=5432        
+        )
+         
+    dbcur = conn.cursor()
+    dbcur.execute(sql)
+    tables = dbcur.fetchall()   
+
+    for title in tables:       
+        sql = f'SELECT * FROM {title[0]}'       
+        dbcur.execute(sql)
+        rows = dbcur.fetchall()     
+        keys = [k[0] for k in dbcur.description]      
+        results = [dict(zip(keys, row)) for row in rows]               
+        with open(f'./data/JSON/{title[0]}.json', 'w') as f:
+            f.write(json.dumps(results, indent=4, sort_keys=True, default=str))
+
+    dbcur.close()   
+     
+    return True
+
+
+
+try:
+    connection()
+    print("Complete")
+except Exception as e:
+    print(e)
+    print("Failure")
+
