e8eb17397c7b11e63931fdfcc675b427b7069f07
infoomics <sbx_94@hotmail.com>
2023-03-21 14:49:27 +0000

    initial placeholders

---
+++ terraform/vars.tf
@@ -0,0 +1,16 @@
+variable "extraction_lambda_name" {
+    type = string
+    default = "extraction-lambda"
+}
+
+
+variable "transform_lambda_name" {
+    type = string
+    default = "transform-lambda"
+}
+
+
+variable "load_lambda_name" {
+    type = string
+    default = "load-lambda"
+}
