e7d62335e4c7a35628ccd5c41bcd2de2b98fdb2a
thaliadevoe <thaliadevoe@gmail.com>
2023-03-21 16:43:09 +0000

    Commits lambda file with extraction_lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,15 +1,10 @@
 resource "aws_lambda_function" "extraction_lambda" {
     function_name = "extraction_lambda"
     runtime = "python3.9"
-    # Name of role for this function goes below
-    role = aws_iam_role.___.arn
-    # Name of the .py file with hndler in goes below
-    handler = "example.py"
-    # Links to a zip file
-
-    # source_code_hash = data.archive_file.lambda.output-
-    
-    # Link to wherever the deployement package for this lambda is kept (the code-bucket)
-    s3_bucket = .
-    s3_key = .
+    role = aws_iam_role.lambda_role.arn
+    # Name of the .py file with handler in goes below 
+    handler = "${path.module}/../src/extraction.py"
+    # Links to a zip file, not a bucket & object
+    filename = "${path.module}/../data/extraction.zip"
+    source_code_hash = data.archive_file.lambda.output-base64sha256
 }
