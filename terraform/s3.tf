b12a105a42e31ceee0c0cdda2e27294b8cf9aeae
Naomi Bilder <naomi@192.168.1.164>
2023-03-21 16:27:06 +0000

    Created 3 S3 buckets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,11 @@
+resource "aws_s3_bucket" "ingested_data_bucket" {
+ bucket_prefix = var.code_bucket_prefix
+}
+
 resource "aws_s3_bucket" "code_bucket" {
-  bucket_prefix = "scrumptious-squad-code-bucket-"
+bucket_prefix = var.code_bucket_prefix
+}
+
+resource "aws_s3_bucket" "processed_data_bucket" {
+bucket_prefix = var.code_bucket_prefix
 }
