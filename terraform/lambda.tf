4e486243b91f2cb483c4967e523587cebc65542e
infoomics <sbx_94@hotmail.com>
2023-03-22 21:06:56 +0000

    use autogenerated log groups instead of duplicating

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -36,17 +36,24 @@ resource "aws_lambda_function" "load_lambda" {
 
 
 resource "aws_cloudwatch_log_group" "extraction_group" {
-    name = "extraction-group"
+    # A log group with this name will be auto-generated when the corresponding 
+    # lambda is executed the first time, so we can just use that instead of 
+    # making duplicate log groups. But we need to explicitly create one at the 
+    # point of terraform plan to attach metric filters properly because 
+    # the auto-generation doesn't happen immediately.
+    name = "/aws/lambda/${var.extract_lambda_name}"
 }
 
 
 resource "aws_cloudwatch_log_group" "transformation_group" {
-    name = "transformation-group"
+    # See aws_cloudwatch_log_group.extraction_group
+    name = "/aws/lambda/${var.transform_lambda_name}"
 }
 
 
 resource "aws_cloudwatch_log_group" "loading_group" {
-    name = "loading-group"
+    # See aws_cloudwatch_log_group.extraction_group
+    name = "/aws/lambda/${var.load_lambda_name}"
 }
 
 
