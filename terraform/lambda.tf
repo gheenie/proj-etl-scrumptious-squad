87c74d23109db2b175a9af31de5b679d39e4ad6a
infoomics <sbx_94@hotmail.com>
2023-03-22 09:42:03 +0000

    cleanup attributes of extraction_lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "extraction_lambda" {
-    function_name = "var.extraction_lambda_name"
+    function_name = var.extraction_lambda_name
     runtime = "python3.9"
     role = aws_iam_role.lambda_role.arn
     # Name of the .py file with handler in goes below 
-    handler = "${path.module}/../src/extract.py"
+    handler = "extraction.someting"
     # Links to a zip file, not a bucket & object
     filename = "${path.module}/../data/extraction.zip"
     source_code_hash = data.archive_file.lambda.output-base64sha256
