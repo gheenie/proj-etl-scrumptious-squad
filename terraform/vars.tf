0cde084ee70f2ccd48ae75e1a75223d89499080f
infoomics <sbx_94@hotmail.com>
2023-03-22 14:49:02 +0000

    create sns and subscription, moved from cli

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,32 +1,32 @@
 variable "extract_lambda_name" {
-    type = string
+    type    = string
     default = "extract-lambda"
 }
 
 
 variable "transform_lambda_name" {
-    type = string
+    type    = string
     default = "transform-lambda"
 }
 
 
 variable "load_lambda_name" {
-    type = string
+    type    = string
     default = "load-lambda"
 }
 
 
 variable "ingested_data_bucket_prefix" {
-    type = string
+    type    = string
     default = "scrumptious-squad-in-data-"
 }
 
 variable "processed_data_bucket_prefix" {
-    type = string
+    type    = string
     default = "scrumptious-squad-pr-data-"
 }
 
-# variable "code_bucket_prefix" {
-#     type = string
-#     default = "scrumptious-squad-co-"
-# }
+variable "sns_group_email" {
+    type    = string
+    default = "scrumptious23@yahoo.com"
+}
