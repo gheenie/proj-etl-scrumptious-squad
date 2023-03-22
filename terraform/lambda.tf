e7af16419852d9c85572d07b02b62b203af9a109
infoomics <sbx_94@hotmail.com>
2023-03-22 11:20:10 +0000

    create placeholders for eventbridge

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -8,3 +8,25 @@ resource "aws_lambda_function" "extraction_lambda" {
     filename = "${path.module}/../data/extraction.zip"
     source_code_hash = data.archive_file.extraction_zip.output_base64sha256
 }
+
+resource "aws_lambda_function" "transform_lambda" {
+    # function_name = var.extraction_lambda_name
+    # runtime = "python3.9"
+    # role = aws_iam_role.lambda_role.arn
+    # # Name of the .py file with handler in goes below 
+    # handler = "extraction.someting"
+    # # Links to a zip file, not a bucket & object
+    # filename = "${path.module}/../data/extraction.zip"
+    # source_code_hash = data.archive_file.extraction_zip.output_base64sha256
+}
+
+resource "aws_lambda_function" "load_lambda" {
+    # function_name = var.extraction_lambda_name
+    # runtime = "python3.9"
+    # role = aws_iam_role.lambda_role.arn
+    # # Name of the .py file with handler in goes below 
+    # handler = "extraction.someting"
+    # # Links to a zip file, not a bucket & object
+    # filename = "${path.module}/../data/extraction.zip"
+    # source_code_hash = data.archive_file.extraction_zip.output_base64sha256
+}
