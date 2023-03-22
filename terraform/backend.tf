afa2d072cad8c610bd46b3824634d6abced5032b
Dinesh <dinesh.nimmagadda@outlook.com>
2023-03-22 09:48:00 +0000

    resolving conflicts

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         # Use the same name as ./tfstate_bucket/s3.tf secrets_bucket
-        bucket = "scrumptious-squad-secrets-bucket-day2"
+        bucket = "scrumptious-squad-secrets-bucket"
         key = "terraform.tfstate"
         region = "us-east-1"
     }
