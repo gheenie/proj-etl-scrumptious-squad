5b00b021668d954a3da3146d60508dcc1f99fcb7
Dinesh <dinesh.nimmagadda@outlook.com>
2023-03-22 10:15:52 +0000

    resolving conflicts

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,3 @@
 resource "aws_s3_bucket" "code_bucket" {
   bucket_prefix = "scrumptious-squad-code-bucket-"
 }
-
-# resource "aws_s3_object" "extract_code" {
-#   bucket = aws_s3_bucket.code_bucket.bucket
-#   key = "extraction.zip"
-#   source = "${path.module}/../data/extraction.zip"
-#   source_hash = data.archive_file.extraction_zip.output_md5
-# }
