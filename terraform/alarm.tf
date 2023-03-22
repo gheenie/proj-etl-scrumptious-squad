ddf7ecc4377d57649e6fe6eef7e11409dd64628c
infoomics <sbx_94@hotmail.com>
2023-03-22 18:56:15 +0000

    reorganise, rename; fix alarm_actions; add dimension to alert_nearing_max_runtime

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -1,4 +1,4 @@
-# Monitor execution time for all functions
+# Monitor execution time for all functions.
 
 resource "aws_cloudwatch_log_metric_filter" "runtime_error" {
   log_group_name = aws_cloudwatch_log_group.integration_group.name
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_log_metric_filter" "runtime_error" {
 
 resource "aws_cloudwatch_metric_alarm" "alert_runtime_errors" {
   metric_name         = aws_cloudwatch_log_metric_filter.runtime_error.metric_transformation[0].name
-  namespace           = aws_cloudwatch_log_metric_filter.runtime_error.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.runtime_error.metric_transformation[0].namespace
 
   alarm_name          = "alert_runtime_errors"
   evaluation_periods  = "1"
@@ -24,104 +24,121 @@ resource "aws_cloudwatch_metric_alarm" "alert_runtime_errors" {
   statistic           = "Sum"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold           = "1"
-  alarm_actions       = [aws_sns_topic_subscription.error_alerts_email_target.arn]
+  alarm_actions       = [aws_sns_topic.error_alerts.arn]
   alarm_description   = "Oh no! We've passed our max runtime!"
 }
 
 resource "aws_cloudwatch_metric_alarm" "alert_nearing_max_runtime" {
-  metric_name = "Duration"
-  namespace = "AWS/Lambda"
+  dimensions = {
+        "FunctionName" = aws_lambda_function.extract_lambda.function_name
+        # Will inserting multiple functions here work?
+    }
+  
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
 
-  alarm_name = "alert_nearing_max_runtime"
+  alarm_name          = "alert_nearing_max_runtime"
+  evaluation_periods  = "1"
+  period              = "60"
+  statistic           = "Maximum"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods = "1"
-  period = "60"
-  statistic = "Maximum"
-  threshold = "600"
-  alarm_actions = [aws_sns_topic_subscription.error_alerts_email_target.arn]
-  alarm_description = "Uh-oh! We're nearing a runtime error!"
+  threshold           = "600"
+  alarm_actions       = [aws_sns_topic.error_alerts.arn]
+  alarm_description   = "Uh-oh! We're nearing a runtime error!"
 }
 
 
-# Data Integrity
+# Monitor for Data Integrity Violation appearing in log text during extraction.
 
-resource "aws_cloudwatch_log_metric_filter" "data-integrity-metric-filter" {
+resource "aws_cloudwatch_log_metric_filter" "data_integrity_metric_filter" {
+  log_group_name = aws_cloudwatch_log_group.extraction_group.name
+  depends_on     = [aws_cloudwatch_log_group.extraction_group]
+
   name           = "data-integrity-metric-filter"
-  log_group_name = "extraction-group"
   pattern        = "Data Integrity Violation"
+
   metric_transformation {
-    name      = "data-integrity-metric"
+    name      = "Data_Integrity_Violated_Count"
     namespace = "LogMetrics"
     value     = "1"
   }
-  depends_on = [aws_cloudwatch_log_group.extraction-group]
 }
-resource "aws_cloudwatch_metric_alarm" "data-integrity-alarm" {
+
+resource "aws_cloudwatch_metric_alarm" "data_integrity_alarm" {
+  metric_name         = aws_cloudwatch_log_metric_filter.data_integrity_metric_filter.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.data_integrity_metric_filter.metric_transformation[0].namespace
+
   alarm_name          = "data-integrity-alarm"
-  metric_name         = "data-integrity-metric"
-  threshold           = "0"
-  statistic           = "Sum"
-  comparison_operator = "GreaterThanThreshold"
-  datapoints_to_alarm = "1"
   evaluation_periods  = "1"
   period              = "60"
-  namespace           = "LogMetrics"
+  datapoints_to_alarm = "1"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "0"
   alarm_description   = "Data integrity violation detected"
-  alarm_actions       = [aws_sns_topic_subscription.error_alerts_email_target.arn]
+  alarm_actions       = [aws_sns_topic.error_alerts.arn]
 }
 
 
-# Validation
+# Monitor for Data Validation Failed appearing in log text during extraction.
 
-resource "aws_cloudwatch_log_metric_filter" "data-validation-metric-filter" {
+resource "aws_cloudwatch_log_metric_filter" "data_validation_metric_filter" {
+  log_group_name = aws_cloudwatch_log_group.extraction_group.name
+  depends_on     = [aws_cloudwatch_log_group.extraction_group]
+
   name           = "data-validation-metric-filter"
-  log_group_name = "extraction-group"
   pattern        = "Data Validation Failed"
+
   metric_transformation {
-    name      = "data-validation-metric"
+    name      = "Data_Validation_Failed_Count"
     namespace = "LogMetrics"
     value     = "1"
   }
-  depends_on = [aws_cloudwatch_log_group.extraction-group]
 }
-resource "aws_cloudwatch_metric_alarm" "data-validation-alarm" {
+
+resource "aws_cloudwatch_metric_alarm" "data_validation_alarm" {
+  metric_name         = aws_cloudwatch_log_metric_filter.data_validation_metric_filter.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.data_validation_metric_filter.metric_transformation[0].namespace
+
   alarm_name          = "data-validation-alarm"
-  metric_name         = "data-validation-metric"
-  threshold           = "0"
-  statistic           = "Sum"
-  comparison_operator = "GreaterThanThreshold"
-  datapoints_to_alarm = "1"
   evaluation_periods  = "1"
   period              = "60"
-  namespace           = "LogMetrics"
+  datapoints_to_alarm = "1"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "0"
   alarm_description   = "Data validation failed"
-  alarm_actions       = [aws_sns_topic_subscription.error_alerts_email_target.arn]
+  alarm_actions       = [aws_sns_topic.error_alerts.arn]
 }
 
 
-# Transformation - -> parquet
+# Monitor for Transformation Error appearing in log text during transformation.
 
-resource "aws_cloudwatch_log_metric_filter" "transformation-error-metric-filter" {
+resource "aws_cloudwatch_log_metric_filter" "transformation_error_metric_filter" {
+  log_group_name = aws_cloudwatch_log_group.transformation_group.name
+  depends_on     = [aws_cloudwatch_log_group.transformation_group]
+
   name           = "transformation-error-metric-filter"
-  log_group_name = "transformation-group"
   pattern        = "Transformation Error"
+
   metric_transformation {
-    name      = "transformation-error-metric"
+    name      = "Transformation_Error_Count"
     namespace = "LogMetrics"
     value     = "1"
   }
-  depends_on = [aws_cloudwatch_log_group.transformation-group]
 }
-resource "aws_cloudwatch_metric_alarm" "transformation-error-alarm" {
+
+resource "aws_cloudwatch_metric_alarm" "transformation_error_alarm" {
+  metric_name         = aws_cloudwatch_log_metric_filter.transformation_error_metric_filter.metric_transformation[0].name
+  namespace           = aws_cloudwatch_log_metric_filter.transformation_error_metric_filter.metric_transformation[0].namespace
+
   alarm_name          = "transformation-error-alarm"
-  metric_name         = "transformation-error-metric"
-  namespace           = "LogMetrics"
-  statistic           = "Sum"
-  period              = "60"
   evaluation_periods  = "1"
-  threshold           = "0"
+  period              = "60"
+  statistic           = "Sum"
   comparison_operator = "GreaterThanThreshold"
-  alarm_actions       = [aws_sns_topic_subscription.error_alerts_email_target.arn]
+  threshold           = "0"
+  alarm_actions       = [aws_sns_topic.error_alerts.arn]
 }
 
 
@@ -135,8 +152,8 @@ resource "aws_cloudwatch_log_metric_filter" "total-runtime-metric-filter" {
     name      = "total-runtime-metric"
     namespace = "LogMetrics"
     value     = "900"
-    }
   }
+}
 resource "aws_cloudwatch_metric_alarm" "total-runtime-alarm" {
   alarm_name          = "total-runtime-alarm"
   metric_name         = "total-runtime-metric"
@@ -148,5 +165,5 @@ resource "aws_cloudwatch_metric_alarm" "total-runtime-alarm" {
   period              = "60"
   namespace           = "LogMetrics"
   alarm_description   = "Total runtime exceeded threshold of 15 minutes"
-  alarm_actions       = [aws_sns_topic_subscription.error_alerts_email_target.arn]
+  alarm_actions       = [aws_sns_topic.error_alerts.arn]
 }
