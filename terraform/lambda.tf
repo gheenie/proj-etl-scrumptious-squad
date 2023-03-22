3aaf40eb1c795488e577c60013d5bd15a6de48a2
infoomics <sbx_94@hotmail.com>
2023-03-22 15:41:56 +0000

    remove old lambda code

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,29 +1,3 @@
-resource "aws_lambda_function" "extraction_lambda" {
-    function_name = "${var.extraction_lambda_name}"
-    runtime = "python3.9"
-    role = aws_iam_role.lambda_role.arn
-    # Name of the .py file with handler in goes below
-    handler = "${path.module}/../src/extract.py"
-    # Links to a zip file, not a bucket & object
-    filename = "${path.module}/../data/extraction.zip"
-    source_code_hash = data.archive_file.extraction_zip.output_base64sha256
-}
-
-resource "aws_cloudwatch_log_group" "extraction-group" {
-  name = "extraction-group"
-}
-
-resource "aws_cloudwatch_log_group" "transformation-group" {
-  name = "transformation-group"
-}
-
-resource "aws_cloudwatch_log_group" "loading-group" {
-  name = "loading-group"
-}
-
-resource "aws_cloudwatch_log_group" "integration-group" {
-  name = "integration-group"
-
 resource "aws_lambda_function" "extract_lambda" {
     function_name = var.extract_lambda_name
     runtime = "python3.9"
@@ -34,6 +8,7 @@ resource "aws_lambda_function" "extract_lambda" {
     filename = "${path.module}/../data/extract.zip"
     source_code_hash = data.archive_file.extraction_zip.output_base64sha256
 }
+
 
 resource "aws_lambda_function" "transform_lambda" {
     function_name = var.transform_lambda_name
@@ -46,6 +21,7 @@ resource "aws_lambda_function" "transform_lambda" {
     source_code_hash = data.archive_file.transform_zip.output_base64sha256
 }
 
+
 resource "aws_lambda_function" "load_lambda" {
     function_name = var.load_lambda_name
     runtime = "python3.9"
@@ -56,4 +32,24 @@ resource "aws_lambda_function" "load_lambda" {
     filename = "${path.module}/../data/load.zip"
     source_code_hash = data.archive_file.load_zip.output_base64sha256
 
+}
+
+
+resource "aws_cloudwatch_log_group" "extraction-group" {
+    name = "extraction-group"
+}
+
+
+resource "aws_cloudwatch_log_group" "transformation-group" {
+    name = "transformation-group"
+}
+
+
+resource "aws_cloudwatch_log_group" "loading-group" {
+    name = "loading-group"
+}
+
+
+resource "aws_cloudwatch_log_group" "integration-group" {
+    name = "integration-group"
 }
