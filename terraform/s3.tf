08675b0e26b75c31bac23d17327539168eae2399
infoomics <sbx_94@hotmail.com>
2023-03-23 16:55:29 +0000

    temporary comment, accidentally deleted bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "ingested_data_bucket" {
-    bucket_prefix = var.ingested_data_bucket_prefix
+    # bucket_prefix = var.ingested_data_bucket_prefix
+    bucket = "scrumptious-squad-in-data-20230323093358275600000004"
 }
 
 resource "aws_s3_bucket" "processed_data_bucket" {
-    bucket_prefix = var.processed_data_bucket_prefix
+    # bucket_prefix = var.processed_data_bucket_prefix
+    bucket = "scrumptious-squad-pr-data-20230323093358336100000005"
 }
