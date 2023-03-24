1228af86f5cdf1f51d72e900b7d7e5a386bcd178
soyborg <alex.leigh.work@gmail.com>
2023-03-24 13:47:00 +0000

    code working

--- src/set_up/read_buck.py
+++ src/set_up/read_buck.py
@@ -1,29 +1,31 @@
 import boto3
-from time import time
 import pandas as pd
 from io import BytesIO
 
-
-s3 = boto3.client('s3')
-response = s3.list_buckets()
-buffer = BytesIO()
-bucketname = [bucket['Name'] for bucket in response['Buckets']][0]
-
-
-files =s3.list_objects_v2(Bucket=bucketname)
-objects = [file['key'] for file in files['contents']]
-
-for filename in files:
-    buffer = BytesIO()
-    client = boto3.resource('s3')
-    
-
-data = s3.get_object(Bucket=bucketname, Key=file['Key'])
-contents = data['Body'].read()
-print(contents)
-print(contents.decode("buffer"))
+def get_parquet(title):
+    bucketname = 'nicebucket1679649834'  
+    s3 = boto3.client('s3')
+    files =s3.list_objects_v2(Bucket=bucketname)
+    filename = f"{title}.parquet"      
+    if filename in [file['Key'] for file in files['Contents']]:       
+        print(filename)    
+        buffer = BytesIO()
+        client = boto3.resource('s3')
+        object=client.Object(bucketname, filename)
+        object.download_fileobj(buffer)
+        df = pd.read_parquet(buffer)
+        return df
 
 
-
-
+def check_table_in_bucket(title):    
+        bucketname = 'nicebucket1679649834'  
+        s3 = boto3.client('s3')
+        files =s3.list_objects_v2(Bucket=bucketname)
+        filename = f"{title[0]}.parquet"  
+        filenames= [file['Key'] for file in files['Contents']]
+        
+        return filename in filenames
+        
+            
+   
 
--- src/set_up/set_up.py
+++ src/set_up/set_up.py
@@ -1,5 +1,5 @@
 from make_secrets import entry as make_secret
-from make_bucket import make_bucket
+from set_up.make_bucket import make_bucket
 
 def index():    
     bucket_name = make_bucket()
