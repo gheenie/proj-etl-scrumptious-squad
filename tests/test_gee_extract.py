f4689f50bb419f69c1dbcf9a1fd0d990fec35b04
infoomics <sbx_94@hotmail.com>
2023-03-24 14:04:37 +0000

    remove explanatory pandas comments

--- tests/test_gee_extract.py
+++ tests/test_gee_extract.py
@@ -9,7 +9,6 @@ pyarrow instead to make use of Parquet's full capabilities.
 
 from src.make_parquet import (index)
 import pandas as pd
-import pyarrow.parquet as pq
 
 
 def test_example():
@@ -20,23 +19,5 @@ def test_example():
     index('config/.env.test')
     # Read one table into a DataFrame.
     sales_order_table = pd.read_parquet(f'{EXTRACTION_SEED_FOLDER}/{SALES_ORDER_FILE}')
-
-    # test_pq = pq.read_table(f'{EXTRACTION_SEED_FOLDER}/{SALES_ORDER_FILE}')
-    # print(test_pq)
     
-    # # Example statements to demonstrate Pandas functions. Refer to NC Notes as well.
-    # # The first square brackets is similar to a WHERE condition in sql.
-    # print(sales_order_table.loc[sales_order_table.sales_order_id == 1416])
-    # # The second square brackets is to filter out unnecessary columns. Note that they're
-    # # nested, unnested gives different results.
-    # print(sales_order_table.loc[sales_order_table.sales_order_id == 1416][['staff_id']])
-    # # The previous result is still in a 'Series'. You need to access the cell with .values
-    # # to turn it into an int that you can assert with. Currently address_table is one column
-    # # because of previous filter, [0] because there's only one row.
-    # print(sales_order_table.loc[sales_order_table.sales_order_id == 1416][['staff_id']].values[0])
-    # print(sales_order_table.loc[sales_order_table.sales_order_id == 1416][['staff_id']].values[0] == 3)
-    
-    # # This is a fake test to show that the assert works cause actual testing is currently failing.
-    # assert sales_order_table.loc[sales_order_table.sales_order_id == 1416][['staff_id']].values[0] == 3
-    # This is the actual test. The fail message is because there isn't a row with sales_order_id == 5.
     assert sales_order_table.loc[sales_order_table.sales_order_id == 5][['staff_id']].values[0] == 2
