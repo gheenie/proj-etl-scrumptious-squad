3387adacda53485dd10f215f5d91df9277bc7666
infoomics <sbx_94@hotmail.com>
2023-03-27 10:25:02 +0100

    use prefix again, 120hr sandbox remade

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,11 @@
 resource "aws_s3_bucket" "ingested_data_bucket" {
-    # bucket_prefix = var.ingested_data_bucket_prefix
-    bucket = "scrumptious-squad-in-data-20230323093358275600000004"
+    bucket_prefix = var.ingested_data_bucket_prefix
+    # Use a fixed name if anything goes wrong with the sandbox
+    # bucket = "scrumptious-squad-in-data-20230323093358275600000004"
 }
 
 resource "aws_s3_bucket" "processed_data_bucket" {
-    # bucket_prefix = var.processed_data_bucket_prefix
-    bucket = "scrumptious-squad-pr-data-20230323093358336100000005"
+    bucket_prefix = var.processed_data_bucket_prefix
+    # Use a fixed name if anything goes wrong with the sandbox
+    # bucket = "scrumptious-squad-pr-data-20230323093358336100000005"
 }
