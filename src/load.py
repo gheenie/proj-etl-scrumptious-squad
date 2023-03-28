ffe4177051d13d04beb0516d9274af6cc8d88eb7
Naomi Bilder <naomi@192.168.1.179>
2023-03-28 10:44:19 +0100

    added new tests & read_data function

--- src/load.py
+++ src/load.py
@@ -1,37 +1,77 @@
 import pandas as pd
+import pyarrow.parquet as pq
 from dotenv import load_dotenv
 import os
-from decouple import config
 import pg8000
+import boto3
+# import awswrangler
+from decouple import config
 
-# converts to human-readable format for error handling
-def read_data(parquet_path):
-    return pd.read_parquet(f"${parquet_path}", engine = "auto")
+
+load_dotenv()
+
+
+# # converts to human-readable format for error handling
+def read_data(PROCESSED_DATA_BUCKET):
+    df = {}
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(PROCESSED_DATA_BUCKET)
+
+    for obj in bucket.objects.all():
+        processed_s3_data = obj.get()['Body'].read()
+        processed_s3_data[obj.key] = df
+    return df
+
+
+print(read_data("scrumptious-squad-pr-data-20230323093358336100000006"))
+
+# s3 = boto3.resource('s3')
+# bucket = s3.Bucket(PROCESSED_DATA_BUCKET)
+# return pd.read_parquet(f"${parquet_path}", engine="auto")
+
+
+# print(PROCESSED_DATA_BUCKET)
+
+
+# def read_s3_data():
+#     processed_s3_data = {}
+#     df = awswrangler.s3.read_parquet(
+#         path=f"s3://{PROCESSED_DATA_BUCKET}/", dataset=True)
+
+#     processed_s3_data = df
+#     return processed_s3_data
+
 
 # Checks data is not corrupted
 def corruption_checker():
     pass
 
 # Make connection to data warehouse
+
+
 def make_warehouse_connection():
     load_dotenv()
-    API_HOST=config('host')
-    API_USER=config('user')
-    API_PASS=config('password')
-    API_DBASE=config('database')
+    API_HOST = config('host')
+    API_USER = config('user')
+    API_PASS = config('password')
+    API_DBASE = config('database')
     conn = pg8000.connect(
-        host = API_HOST,
-        user = API_USER,
-        password = API_PASS,
-        database = API_DBASE
+        host=API_HOST,
+        user=API_USER,
+        password=API_PASS,
+        database=API_DBASE
     )
     return conn
-    
+
 # Pushes parqueted data to data warehouse
+
+
 def insert_into_warehouse():
     pass
 
 # Integrated function to combine all of the above
+
+
 def load_to_warehouse():
     try:
         # Log status message to confirm load
@@ -41,5 +81,7 @@ def load_to_warehouse():
         pass
 
 # Lambda_handler
+
+
 def lambda_handler():
     pass
