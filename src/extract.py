5e07afde3f8243b4a5860ad170189b2422e01807
infoomics <sbx_94@hotmail.com>
2023-03-29 15:26:49 +0100

    remove print()s meant for debugging

--- src/extract.py
+++ src/extract.py
@@ -131,7 +131,6 @@ def get_most_recent_time(title):
 
     #table = pd.read_parquet(f"./database-access/data/parquet/{title[0]}.parquet", engine='pyarrow')
     table = get_parquet(title[0]) 
-    print(table)
        
 
     #compile a sorted list of 'last_updated' values and another sorted list of 'created_at' values existing inside previous readings
@@ -154,7 +153,6 @@ def check_each_table(tables, dbcur):
 
     for title in tables:                      
         rows, keys = get_table(dbcur, title)
-        if title == ['sales_order']: print(rows)
         
         #if there are no existing parquet files storing our data, create them
         if not check_table_in_bucket(title): 
