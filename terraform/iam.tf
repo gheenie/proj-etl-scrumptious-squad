ef3b3a522c3bf445e51e4d18a24aea7ce1e90a1a
infoomics <sbx_94@hotmail.com>
2023-03-30 16:55:40 +0100

    reorganise

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-    name_prefix = "lambda-role-"
-
+    name_prefix        = "lambda-role-"
     assume_role_policy = <<EOF
     {
         "Version": "2012-10-17",
@@ -24,36 +23,33 @@ resource "aws_iam_role" "lambda_role" {
 
 resource "aws_iam_policy" "read_from_s3_policy" {
     name_prefix = "read-from-s3-policy-"
-    policy = data.aws_iam_policy_document.read_from_s3_document.json
+    policy      = data.aws_iam_policy_document.read_from_s3_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
+    role       = aws_iam_role.lambda_role.name
+    policy_arn = aws_iam_policy.read_from_s3_policy.arn
 }
 
 
 resource "aws_iam_policy" "log_to_cloudwatch_policy" {
     name_prefix = "log-to-cloudwatch-policy-"
-    policy = data.aws_iam_policy_document.log_to_cloudwatch_document.json
+    policy      = data.aws_iam_policy_document.log_to_cloudwatch_document.json
 }
-
-
-resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.read_from_s3_policy.arn
-}
-
 
 resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
+    role       = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.log_to_cloudwatch_policy.arn
 }
 
 
 resource "aws_iam_policy" "access_secretsmanager_policy" {
     name_prefix = "access-secretsmanager-policy-"
-    policy = data.aws_iam_policy_document.access_secretsmanager_document.json
+    policy      = data.aws_iam_policy_document.access_secretsmanager_document.json
 }
 
-
 resource "aws_iam_role_policy_attachment" "lambda_secretsmanager_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
+    role       = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.access_secretsmanager_policy.arn
 }
 
@@ -61,13 +57,12 @@ resource "aws_iam_role_policy_attachment" "lambda_secretsmanager_policy_attachme
 
 resource "aws_iam_policy" "allow_all_s3_actions_policy" {
     name_prefix = "allow-all-s3-actions-policy-"
-    policy = data.aws_iam_policy_document.allow_all_s3_actions_document.json
+    policy      = data.aws_iam_policy_document.allow_all_s3_actions_document.json
 }
-
 
 # This will allow the lambda to upload .parquet files to both our buckets,
 # but it's not following the principle of least privileges.
 resource "aws_iam_role_policy_attachment" "lambda_allow_all_s3_actions_policy_attachment" {
-    role = aws_iam_role.lambda_role.name
+    role       = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.allow_all_s3_actions_policy.arn
 }
