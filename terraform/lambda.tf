79f2dacc010e647ec98424cb0fc5c44f940bd9fa
infoomics <sbx_94@hotmail.com>
2023-03-30 18:06:30 +0100

    increase memory size and add pandas layer to all lambdas; restyle

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,37 +1,42 @@
 resource "aws_lambda_function" "extract_lambda" {
-    function_name = var.extract_lambda_name
-    runtime = "python3.9"
-    role = aws_iam_role.lambda_role.arn
+    function_name    = var.extract_lambda_name
+    runtime          = "python3.9"
+    role             = aws_iam_role.lambda_role.arn
     # Name of the .py file with handler in goes below 
-    handler = "extract.someting"
+    handler          = "extract.someting"
     # Links to a zip file, not a bucket & object
-    filename = var.extract_archive_output_path
+    filename         = var.extract_archive_output_path
     source_code_hash = data.archive_file.extract_zip.output_base64sha256
-    layers = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:1"]
+    layers           = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:1"]
+    memory_size      = 512
 }
 
 
 resource "aws_lambda_function" "transform_lambda" {
-    function_name = var.transform_lambda_name
-    runtime = "python3.9"
-    role = aws_iam_role.lambda_role.arn
+    function_name    = var.transform_lambda_name
+    runtime          = "python3.9"
+    role             = aws_iam_role.lambda_role.arn
     # Name of the .py file with handler in goes below 
-    handler = "transform.something"
+    handler          = "transform.something"
     # Links to a zip file, not a bucket & object
-    filename = var.transform_archive_output_path
+    filename         = var.transform_archive_output_path
     source_code_hash = data.archive_file.transform_zip.output_base64sha256
+    layers           = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:1"]
+    memory_size      = 512
 }
 
 
 resource "aws_lambda_function" "load_lambda" {
-    function_name = var.load_lambda_name
-    runtime = "python3.9"
-    role = aws_iam_role.lambda_role.arn
+    function_name    = var.load_lambda_name
+    runtime          = "python3.9"
+    role             = aws_iam_role.lambda_role.arn
     # Name of the .py file with handler in goes below 
-    handler = "load.something"
+    handler          = "load.something"
     # Links to a zip file, not a bucket & object
-    filename = var.load_archive_output_path
+    filename         = var.load_archive_output_path
     source_code_hash = data.archive_file.load_zip.output_base64sha256
+    layers           = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:1"]
+    memory_size      = 512
 
 }
 
