4ca01988886e80e2cd1f70923768315fda8aa5b5
infoomics <sbx_94@hotmail.com>
2023-03-31 11:31:34 +0100

    separate out src copying into separate processes

--- terraform/shell_scripting.tf
+++ terraform/shell_scripting.tf
@@ -1,14 +1,55 @@
 resource "null_resource" "install_dependencies" {
     provisioner "local-exec" {
         command = <<-EOT
-            rm -r ./../data/src
-            cp -r ./../src ./../data/src
-            pip install pg8000 -t ./../data/src
-            pip install python-dotenv -t ./../data/src
+            pip install pg8000 -t ./../data/src_extract
+            pip install python-dotenv -t ./../data/src_extract
             EOT
     }
 
     triggers = {
         dependencies_versions = filemd5("./../requirements.txt")
+    }
+}
+
+
+resource "null_resource" "copy_src_extract" {
+    provisioner "local-exec" {
+        # cp will not work if folder doesn't exist, so mkdir first.
+        # mkdir will not overwrite existing folders. -p flag will
+        # prevent erroring if folder exists.
+        command = <<-EOT
+            mkdir -p ./../data/src_extract
+            cp -r ./../src/extract.py ./../data/src_extract/extract.py
+            EOT
+    }
+
+    triggers = {
+        src_file_hash = filemd5("./../src/extract.py")
+    }
+}
+
+resource "null_resource" "copy_src_transform" {
+    provisioner "local-exec" {
+        command = <<-EOT
+            mkdir -p ./../data/src_transform
+            cp -r ./../src/transform.py ./../data/src_transform/transform.py
+            EOT
+    }
+
+    triggers = {
+        src_file_hash = filemd5("./../src/transform.py")
+    }
+}
+
+resource "null_resource" "copy_src_load" {
+    provisioner "local-exec" {
+        command = <<-EOT
+            mkdir -p ./../data/src_load
+            cp -r ./../src/load.py ./../data/src_load/load.py
+            EOT
+    }
+
+    triggers = {
+        src_file_hash = filemd5("./../src/load.py")
     }
 }
