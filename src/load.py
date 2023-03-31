5e5415cb3c03690498fc0178a19c0c5c11504f65
thaliadevoe <thaliadevoe@gmail.com>
2023-03-31 09:17:05 +0100

    Adds warehouse push and lambda handler with testing for the load.py function

--- src/load.py
+++ src/load.py
@@ -1,61 +1,103 @@
-import pandas as pd
 from dotenv import load_dotenv
-from os.path import join, dirname
 import os
 import pg8000
 import boto3
 from pathlib import Path
-# import awswrangler
+import io
+import pyarrow.parquet as pq
 
 
-dotenv_path = join(dirname(__file__), '../config/.env')
-load_dotenv(dotenv_path)
-
-# Gives the access to the bucket data
-
-
-def get_data(bucket):
-    s3_client = boto3.client("s3")
-    bucket = s3_client.list_objects_v2(Bucket = bucket)
-    return bucket
-
-
-# Make connection to data warehouse
-def make_warehouse_connection():
-    dotenv_path = Path('./config/.env.data_warehouse')
-    load_dotenv(dotenv_path)
-    API_HOST =  os.environ["host"]
-    API_USER = os.environ["user"]
-    API_PASS = os.environ["password"]
-    API_DBASE = os.environ["database"]
-    conn = pg8000.connect(
-        host=API_HOST,
-        user=API_USER,
-        password=API_PASS,
-        database=API_DBASE
-    )
-    return conn
+def get_data(bucket_name, file_path):
+    s3 = boto3.client('s3')
+    objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=file_path)['Contents']
+    dfs = {}
+    for obj in objects:
+        key = obj['Key']
+        filename = key.split('/')[-1].split('.')[0]
+        obj = s3.get_object(Bucket=bucket_name, Key=key)
+        buffer = io.BytesIO(obj['Body'].read())
+        table = pq.read_table(buffer)
+        df = table.to_pandas()
+        dfs[f"df_{filename}"] = df
+    return dfs
 
 
-# Pushes parqueted data to data warehouse
-
-
-def insert_into_warehouse():
-    pass
-
-# Integrated function to combine all of the above
-
-
-def load_to_warehouse():
+def make_warehouse_connection(dotenv_path="./config/.env.data_warehouse"):
     try:
-        # Log status message to confirm load
-        pass
-    except:
-        # Log status with helpful error message
-        pass
+        dotenv = Path(dotenv_path)
+        load_dotenv(dotenv)
+        API_HOST = os.environ["host"]
+        API_USER = os.environ["user"]
+        API_PASS = os.environ["password"]
+        API_DBASE = os.environ["database"]
+        conn = pg8000.connect(
+            host=API_HOST,
+            user=API_USER,
+            password=API_PASS,
+            database=API_DBASE
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to the data warehouse: {str(e)}")
+        return None
 
-# Lambda_handler
+
+def load_to_warehouse(bucket_name, file_path, dotenv_path):
+    try:
+        dfs = get_data(bucket_name, file_path)
+        conn = make_warehouse_connection(dotenv_path)
+        if conn is None:
+            return "Error: Unable to connect to the data warehouse"
+        cur = conn.cursor()
+        for table in dfs:
+            table_name = table[3:]
+            print(f"Loading table {table_name}")
+            for index, row in dfs[table].iterrows():
+                values = ', '.join(['%s'] * len(row))
+                columns = ', '.join(row.index)
+                sql = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
+                cur.execute(sql, tuple(row))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return "Successfully loaded into data warehouse"
+    except Exception as e:
+        print(f"Error loading data to the warehouse: {str(e)}")
+        return None
 
 
-def lambda_handler():
-    pass
+def lambda_handler(event, context):
+    bucket_name = event['bucket_name']
+    file_path = event['file_path']
+    dotenv_path = event['dotenv_path']
+
+    try:
+        dfs = get_data(bucket_name, file_path)
+        conn = make_warehouse_connection(dotenv_path)
+        if conn is None:
+            return {
+                'statusCode': 500,
+                'body': 'Error: Unable to connect to the data warehouse'
+            }
+        cur = conn.cursor()
+        for table in dfs:
+            table_name = table[3:]
+            print(f"Loading table {table_name}")
+            for index, row in dfs[table].iterrows():
+                values = ', '.join(['%s'] * len(row))
+                columns = ', '.join(row.index)
+                sql = f"INSERT INTO {table_name} ({columns}) VALUES ({values})"
+                cur.execute(sql, tuple(row))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return {
+            'statusCode': 200,
+            'body': 'Successfully loaded into data warehouse'
+        }
+    except Exception as e:
+        print(f"Error loading data to the warehouse: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': f'Error loading data to the warehouse: {str(e)}'
+        }
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,20 +1,13 @@
-from src.load import get_data
+from unittest.mock import patch, Mock
+from src.load import get_data, make_warehouse_connection, load_to_warehouse, lambda_handler
 from moto import mock_s3
-# from unittest.mock import pandas as pd
-from unittest.mock import patch
-from unittest.mock import Mock
 import pytest
 import boto3
-import botocore
-from pathlib import Path
 import os
+import io
 import pandas as pd
 import pyarrow.parquet as pq
-from pyarrow import fs
 import pg8000
-# import awswrangler
-
-# Establishing mock AWS credentials
 @pytest.fixture(scope='function')
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
@@ -23,61 +16,92 @@ def aws_credentials():
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
-
-
-# Tests for get_data subfunction
 @mock_s3
 def test_creating_mock_s3():
     conn = boto3.client("s3", region_name="us-east-1")
     res = conn.create_bucket(Bucket="test_bucket_29")
     assert res['ResponseMetadata']['HTTPStatusCode'] == 200
-
 @mock_s3
-def test_accessing_the_objects_in_the_bucket():
-    conn = boto3.client("s3", region_name="us-east-1")
-    res = conn.create_bucket(Bucket="test_bucket_29")
-    parquet_files = get_data('test_bucket_29')
-    assert parquet_files['KeyCount'] == 0
-
+def test_get_data():
+    readtable = pq.read_table('./load_test_db/dim_date.parquet')
+    expectedfile = readtable.to_pandas()
+    bucket_name = "test_bucket_29"
+    file_path = "data/parquet/"
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket=bucket_name)
+    # Uploading test files to mock bucket
+    s3.Object(bucket_name, f"{file_path}/dim_date.parquet").put(
+        Body=open('./load_test_db/dim_date.parquet', 'rb'))
+    s3.Object(bucket_name, f"{file_path}/hello.parquet").put(
+        Body=open('./load_test_db/hello_test.parquet', 'rb'))
+    dfs = get_data(bucket_name, file_path)
+    pd.testing.assert_frame_equal(expectedfile, dfs["df_dim_date"])
 @mock_s3
-def test_retreiving_objects_in_the_bucket():
-    bucket = "test_bucket_29"
-    conn = boto3.client("s3")
-    res = conn.create_bucket(Bucket=bucket)
-    parquet_files = get_data(bucket)
-    # uploading some fake data to bucket
-    with open("./load_test_db/hello_test.parquet", "rb") as f:
-        conn.upload_fileobj(f, f"{bucket}", "hello.parquet")
-    parquet_files = get_data(bucket)
-    assert parquet_files['KeyCount'] == 1
-    
+def test_get_data_dim_location():
+    readtable = pq.read_table('./load_test_db/dim_currency.parquet')
+    expectedfile = readtable.to_pandas()
+    bucket_name = "test_bucket_29"
+    file_path = "data/parquet/"
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket=bucket_name)
+    s3.Object(bucket_name, f"{file_path}/dim_currency.parquet").put(
+        Body=open('./load_test_db/dim_currency.parquet', 'rb'))
+    dfs = get_data(bucket_name, file_path)
+    pd.testing.assert_frame_equal(expectedfile, dfs["df_dim_currency"])
+def test_connection_to_warehouse():
+    connect = make_warehouse_connection("./config/.env.test")
+    assert isinstance(connect, pg8000.Connection)
 @mock_s3
-def test_retreiving_objects_in_the_bucketss():
-    bucket ="rand2"
-    s3_client = boto3.client("s3")
-    s3_client.create_bucket(Bucket=bucket)
-    with open("./load_test_db/hello_test.parquet", "rb") as f:
-        s3_client.upload_fileobj(f, f"{bucket}", "hello.parquet")
-    bucket_objects = s3_client.get_object(Bucket =bucket, Key = "hello.parquet")
-    print(bucket_objects)
-
-    file_paths = [f's3://{bucket}/{obj["Key"]}' for obj in bucket_objects['Contents']]
-    for file_path in file_paths:
-        s3_file = pq.ParquetDataset(file_path)
-        table = s3_file.read().to_pandas()
-        df= pd.DataFrame(table)
-    assert df.columns.tolist() == ['hello']
-
-
-def test_load_to_warehouse_can_push_to_warehouse():
-    pass
-
-
+@patch('src.load.make_warehouse_connection')
+def test_load_to_warehouse(mock_make_connection):
+    bucket_name = "test_bucket_31"
+    file_path = "data/parquet/"
+    dotenv_path = './config/.env.test'
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket=bucket_name)
+    s3.Object(bucket_name, f"{file_path}/dim_currency.parquet").put(
+        Body=open('./load_test_db/dim_currency.parquet', 'rb'))
+    dfs = get_data(bucket_name, file_path)
+    result = load_to_warehouse(bucket_name, file_path, dotenv_path)
+    assert result == 'Successfully loaded into data warehouse'
+def test_load_to_warehouse_inserted_values():
+    dotenv_path = './config/.env.test'
+    conn = make_warehouse_connection(dotenv_path)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM dim_currency")
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    assert rows[0] == [0, 'GBP', 'Pound']
+    assert rows[1] == [1, 'USD', 'Dollar']
+    assert columns == ['currency_id', 'currency_code', 'currency_name']
+@mock_s3
+def test_lambda_handler():
+    bucket_name = "test_bucket_31"
+    file_path = "data/parquet/"
+    dotenv_path = './config/.env.test'
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket=bucket_name)
+    s3.Object(bucket_name, f"{file_path}/dim_currency.parquet").put(
+        Body=open('./load_test_db/dim_currency.parquet', 'rb'))
+    mock_cursor = Mock()
+    mock_connection = Mock()
+    mock_connection.cursor.return_value = mock_cursor
+    with patch('src.load.make_warehouse_connection', return_value=mock_connection):
+        result = lambda_handler({'bucket_name': bucket_name, 'file_path': file_path, 'dotenv_path': dotenv_path}, None)
+        assert 'Successfully loaded into data warehouse' in result['body']
+@mock_s3
+def test_lambda_handler_error():
+    mock_cursor = Mock()
+    mock_connection = Mock()
+    mock_connection.cursor.return_value = mock_cursor
+    with patch('src.load.make_warehouse_connection', return_value=mock_connection):
+        result = lambda_handler({'bucket_name': 'test-bucket', 'file_path': 'test-data.parquet', 'dotenv_path':'./config/.env.test'}, None)
+        assert 'Error loading data to the warehouse' in result['body']
+        mock_cursor.execute.assert_not_called()
+        mock_connection.commit.assert_not_called()
+        mock_cursor.close.assert_not_called()
+        mock_connection.close.assert_not_called()
 def test_load_to_warehouse_throws_helpful_error_on_incorrect_format():
     pass
-
-
 def test_another_fail_case():
     pass
-
-# Tests for AWS Cloudwatch (possible?)
